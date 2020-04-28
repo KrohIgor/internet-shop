3559e9dbdda94f0e373ec5b84cd93ecfffc26ca2
ihor <krohmal.igor777@gmail.com>
2020-04-28 12:59:41 +0300

    added create ShoppingCart when registering a user

--- src/main/java/mate/academy/internet/shop/controllers/RegistrationController.java
+++ src/main/java/mate/academy/internet/shop/controllers/RegistrationController.java
@@ -6,12 +6,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.academy.internet.shop.lib.Injector;
+import mate.academy.internet.shop.model.ShoppingCart;
 import mate.academy.internet.shop.model.User;
+import mate.academy.internet.shop.service.ShoppingCartService;
 import mate.academy.internet.shop.service.UserService;
 
 public class RegistrationController extends HttpServlet {
     private static Injector injector = Injector.getInstance("mate.academy.internet.shop");
     private UserService userService = (UserService) injector.getInstance(UserService.class);
+    private final ShoppingCartService shoppingCartService =
+            (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -30,6 +34,8 @@ public class RegistrationController extends HttpServlet {
         if (password.equals(repeatPassword)) {
             User user = new User(name, login, password);
             userService.create(user);
+            ShoppingCart userShoppingCart = new ShoppingCart(user);
+            shoppingCartService.create(userShoppingCart);
             resp.sendRedirect(req.getContextPath() + "/");
         } else {
             req.setAttribute("message", "Your password and repeat password aren't the same.");
